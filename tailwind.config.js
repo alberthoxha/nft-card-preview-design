75a2713108ed413cfe4abbb120166ff08bdd8917
Albert Hoxha <alberthoxha444@gmail.com>
2023-05-03 23:15:05 +0200

    firt commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      outfit: "'Outfit'",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
